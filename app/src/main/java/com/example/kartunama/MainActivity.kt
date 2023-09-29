1a60e93271f7581ba2c23729fa0586e1dd3a04bd
ilhamm017 <ilhammuliawan26@gmail.com>
2023-09-29 17:17:36 +0700

    Menambahkan fungsi Home yang menampilkan tampilan home aplikasi

--- app/src/main/java/com/example/kartunama/MainActivity.kt
+++ app/src/main/java/com/example/kartunama/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -39,7 +40,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    
+                    Home()
                 }
             }
         }
@@ -116,8 +117,29 @@ fun Contact() {
         }
     }
 }
+
+@Composable
+fun Home() {
+    Column(
+        modifier = Modifier.padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Profil()
+        }
+        Contact()
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun contactpv() {
-
+fun Homepv() {
+    Home()
 }
