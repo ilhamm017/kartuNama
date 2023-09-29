9a65917ba0d5e70459e79c7a4b1e0cbe5a489843
ilhamm017 <ilhammuliawan26@gmail.com>
2023-09-29 16:20:38 +0700

    Menambahkan fungsi profil yang akan menampilkan profil utama dari aplikasi

--- app/src/main/java/com/example/kartunama/MainActivity.kt
+++ app/src/main/java/com/example/kartunama/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.kartunama
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.kartunama.ui.theme.KartuNamaTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    
                 }
             }
         }
@@ -30,17 +41,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun profil() {
+    Column(
+        modifier = Modifier.padding(12.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.foto_profil),
+            contentDescription = "foto profil",
+            Modifier.size(80.dp).clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Text(
+            text = "Ilham Muliawan"
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Text(
+            text = "Android Developer"
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    KartuNamaTheme {
-        Greeting("Android")
+fun profilpv() {
+    Column(
+        modifier = Modifier.padding(12.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.foto_profil),
+            contentDescription = "foto profil",
+            Modifier.size(80.dp).clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Text(
+            text = "Ilham Muliawan"
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Text(
+            text = "Android Developer"
+        )
     }
 }
